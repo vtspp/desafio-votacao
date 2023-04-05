fe02b2faed066feff26d131938067dc8498c121d
Victor Pinho <vtsppinho@gmail.com>
2023-04-04 22:46:34 -0300

    refactore: Alteração de nome

--- ms-cooperativismo-pauta/src/main/java/br/com/cooperativismo/mapper/MeetingAgendaMapper.java
+++ ms-cooperativismo-pauta/src/main/java/br/com/cooperativismo/mapper/MeetingAgendaMapper.java
@@ -1,13 +1,13 @@
 package br.com.cooperativismo.mapper;
 
 import br.com.cooperativismo.domain.MeetingAgenda;
-import br.com.cooperativismo.event.MeetingAgendaEvent;
+import br.com.cooperativismo.event.MeetingAgendaCreateEvent;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class MeetingAgendaMapper {
 
-    public MeetingAgenda mapperToEntity(MeetingAgendaEvent event) {
+    public MeetingAgenda mapperToEntity(MeetingAgendaCreateEvent event) {
             MeetingAgenda meetingAgenda = new MeetingAgenda();
             meetingAgenda.setId(event.getId());
             meetingAgenda.setName(event.getName());
