17b999292f156fc48e76eef9eb2822784c4660ff
Victor Pinho <vtsppinho@gmail.com>
2023-04-05 10:10:19 -0300

    feature: Implementado manipulador de erro

--- ms-cooperativismo-pauta/src/main/java/br/com/cooperativismo/controller/MeetingAgendaController.java
+++ ms-cooperativismo-pauta/src/main/java/br/com/cooperativismo/controller/MeetingAgendaController.java
@@ -2,9 +2,12 @@ package br.com.cooperativismo.controller;
 
 import br.com.cooperativismo.command.MeetingAgendaCreateCommand;
 import br.com.cooperativismo.service.MeetingAgendaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -14,9 +17,12 @@ public final class MeetingAgendaController {
 
     private final MeetingAgendaService service;
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Mono<MeetingAgendaCreateResponse> createMeetingAgenda(@RequestBody MeetingAgendaCreateCommand command) {
-        return service.createMeetingAgenda(command);
+    public Mono<ResponseEntity<MeetingAgendaCreateResponse>> createMeetingAgenda(@Valid @RequestBody Mono<MeetingAgendaCreateCommand> command) {
+        return service.createMeetingAgenda(command)
+                .map(response -> ResponseEntity.status(HttpStatus.CREATED).body(response))
+                .onErrorResume(WebExchangeBindException.class,
+                        e -> Mono.just(ResponseEntity.status(e.getStatusCode())
+                                        .body(new MeetingAgendaCreateResponse(null,e.getFieldError().getDefaultMessage()))));
     }
 }
