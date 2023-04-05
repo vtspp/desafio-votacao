d625ae541805fa6ce1582a57303da354c7c2424f
Victor Pinho <vtsppinho@gmail.com>
2023-04-05 14:18:25 -0300

    feature: Classe responsável por receber os comandos e gerar eventos para o broker Kafka

---
+++ ms-cooperativismo-sessao/src/main/java/br/com/cooperativismo/service/VotingSessionService.java
@@ -0,0 +1,37 @@
+package br.com.cooperativismo.service;
+
+import br.com.cooperativismo.command.VotingSessionCommand;
+import br.com.cooperativismo.controller.VotingSessionResponse;
+import br.com.cooperativismo.event.VotingSessionEvent;
+import br.com.cooperativismo.helper.DateTimeHelper;
+import br.com.cooperativismo.message.VotingSessionMessageProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public final class VotingSessionService {
+
+    private final VotingSessionMessageProducer messageProducer;
+
+    public Mono<VotingSessionResponse> receiverCommand(Mono<VotingSessionCommand> command) {
+        return command
+                .doOnNext(c -> log.info("Voting Session Service receiveCommand={}, localDateTime={}", c, DateTimeHelper.LOCAL_DATE_TIME_FORMATTED))
+                .map(this.generateEvent())
+                .doOnNext(messageProducer::send)
+                .map(event -> new VotingSessionResponse(event.id()));
+    }
+
+    private Function<VotingSessionCommand, VotingSessionEvent> generateEvent() {
+        return command -> VotingSessionEvent.builder()
+                .id(UUID.randomUUID())
+                .minutesOfDuration(command.minutesOfDuration())
+                .build();
+    }
+}
