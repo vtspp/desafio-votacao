5e941f1aaf1109d06bf060f05339dcb546915beb
Victor Pinho <vtsppinho@gmail.com>
2023-04-05 10:09:17 -0300

    feature: Inclusão de anotação de validação

--- ms-cooperativismo-pauta/src/main/java/br/com/cooperativismo/command/MeetingAgendaCreateCommand.java
+++ ms-cooperativismo-pauta/src/main/java/br/com/cooperativismo/command/MeetingAgendaCreateCommand.java
@@ -1,9 +1,11 @@
 package br.com.cooperativismo.command;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MeetingAgendaCreateCommand {
+    @NotBlank(message = "Field 'Meeting Agenda' is mandatory")
     private String name;
 }
