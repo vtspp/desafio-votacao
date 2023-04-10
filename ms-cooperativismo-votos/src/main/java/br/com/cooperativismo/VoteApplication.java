dd9f2215f2d16c31a3a71416aa936e359b19e119
Victor Pinho <vtsppinho@gmail.com>
2023-04-10 09:43:51 -0300

    feature: Commit da estrutura do projeto

---
+++ ms-cooperativismo-votos/src/main/java/br/com/cooperativismo/VoteApplication.java
@@ -0,0 +1,14 @@
+package br.com.cooperativismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class VoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteApplication.class, args);
+	}
+}
