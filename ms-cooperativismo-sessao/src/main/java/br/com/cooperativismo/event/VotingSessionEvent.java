7d29b8a3121d7f318f05ecaeace5fb214abf8491
Victor Pinho <vtsppinho@gmail.com>
2023-04-10 03:48:43 -0300

    refactor: Organização do código

--- ms-cooperativismo-sessao/src/main/java/br/com/cooperativismo/event/VotingSessionEvent.java
+++ ms-cooperativismo-sessao/src/main/java/br/com/cooperativismo/event/VotingSessionEvent.java
@@ -1,10 +1,8 @@
 package br.com.cooperativismo.event;
 
 import br.com.cooperativismo.enumerate.VotingSessionStatus;
-import lombok.Builder;
 
 import java.util.UUID;
 
-@Builder
 public record VotingSessionEvent(UUID id, int minutesOfDuration, VotingSessionStatus sessionStatus) {
 }
