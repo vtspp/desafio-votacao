fe05a870530d4fad846e89db25c164507f8bd3fc
Victor Pinho <vtsppinho@gmail.com>
2023-05-13 16:07:07 -0300

    refactor: Melhoria no rastreamento de informações logadas

--- ms-cooperativismo-votos/src/main/java/br/com/cooperativismo/message/VoteMessageProducer.java
+++ ms-cooperativismo-votos/src/main/java/br/com/cooperativismo/message/VoteMessageProducer.java
@@ -13,10 +13,10 @@ public record VoteMessageProducer(KafkaTemplate<String, VoteEvent> kafkaTemplate
     public void send(VoteEvent event) {
         try {
             this.kafkaTemplate.send(kafkaTemplate.getDefaultTopic(), event);
-            log.info("Vote-Message-Producer time={} status=SUCCESS method=#send event={}", DateTimeHelper.getLocalDateTimeFormatted(), event);
+            log.info("Vote-Message-Producer time={} status=SUCCESS method=#send event={}", DateTimeHelper.getLocalDateTimeFormatted(), event.getClass().getName());
             // envia métrica de sucesso
         } catch (RuntimeException e) {
-            log.error("Vote-Message-Producer time={} status=ERROR method=#send event={} errorMessage={}", DateTimeHelper.getLocalDateTimeFormatted(), event, e.getMessage());
+            log.error("Vote-Message-Producer time={} status=ERROR method=#send event={} errorMessage={}", DateTimeHelper.getLocalDateTimeFormatted(), event.getClass().getName(), e.getMessage());
             // enviar métrica de erro
             throw e;
         }
